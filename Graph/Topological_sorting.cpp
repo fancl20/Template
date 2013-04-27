f3da28a35b2c5e092d1daf6102d69558fc2b7052
fancl20 <fancl20@gmail.com>
2013-04-27 15:57:43 +0800

    rewrite topsort (without test)

--- Graph/Topological_sorting.cpp
+++ Graph/Topological_sorting.cpp
@@ -1,64 +1,41 @@
 #include <iostream>
 #include <algorithm>
+#include <queue>
 #define MAXEDGE 1000
 #define MAXVERTEX 1000
 using namespace std;
+const int MAXV = 1000;
+const int MAXE = 1000000;
 struct Edge {
-  int p;
-  int next;
-};
-class Foward_Star {
-  public:
-    Foward_Star() {clear();}
-    int add(int from,int to) {
-      edge[last_p].p = to;
-      edge[last_p].next = vertex[from];
-      return vertex[from] = last_p++;
-    }
-    int next_edge(int cur) {return edge[cur].next;}
-    int get_edge(int p) {return vertex[p];}
-    void clear() {
-      for(int i(0);i!=MAXVERTEX;++i) vertex[i] = -1;
-      last_p = 0;
-    }
-    Edge edge[MAXEDGE+10];
-  private:
-    int vertex[MAXVERTEX+10];
-    int last_p;
-};
-
-Foward_Star map;
-int in_degree[MAXVERTEX];
-int result[MAXVERTEX]; //0 degree points
-int p;
-bool topsort(int m) {
-  p = 0;
-  fill_n(result,m,-1);
-  for(int i(0);i!=m;++i)
-    if(in_degree[i]==0) result[p++] = i;
-  for(int q(0);q!=m && result[q]!=-1;++q) {
-    for(int i(map.get_edge(result[q]));i!=-1;i = map.edge[i].next) {
-      int cur(map.edge[i].p);
-      if(--in_degree[cur]==0)
-        result[p++] = cur;
-    }
-  }
-  if(p!=m) return false;
-  else return true;
+	int v;
+	Edge *next;
+} *vertex[MAXV],edge[MAXE],*cur;
+int res[MAXV],deg[MAXV];
+void clear() {
+	memset(deg,0,sizeof deg);
+	memset(vertex,0,sizeof vertex);
+	cur = edge;
+}
+void add(int u,int v) {
+	++deg[v];
+	cur->v = v;
+	cur->next = vertex[u];
+	vertex[u] = cur++;
+}
+bool topsort(int n) {
+  int ptr(0);
+  for(int i(0);i!=n;++i)
+		if(deg[i]==0)
+			res[ptr++] = i;
+	for(int i(0);i!=ptr;++i) {
+		int u = res[i];
+		for(Edge *e = vertex[u];e;e = e->next) {
+			if(--deg[e->v]==0)
+				res[ptr++] = e->v;
+		}
+	}
+	return ptr==n;
 }
 main() {
-  int m,temp;
-  cin>>m;
-  for(int i(0);i!=m;++i) {
-    while (cin>>temp) {
-      if(temp==0) break;
-      map.add(i,temp-1);
-      /**/
-      ++in_degree[temp-1];
-    }
-  }
-  topsort(m);
-  for(int i(0);i!=m;++i) cout<<result[i]+1<<" ";
-  cout<<endl;
-  return 0;
+	return 0;
 }
